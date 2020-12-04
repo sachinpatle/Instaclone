40dd33c08d5cf7407d9aff1d876c5027144ed0c6
sachinpatle <sachinpatle189@gmail.com>
2020-12-04 21:17:12 +0530

    last version

--- routes/user.js
+++ routes/user.js
@@ -64,6 +64,16 @@ router.put('/unfollow',requireLogin,(req,res)=>{
     })
 })
 
-
+router.put("/updatepic",requireLogin,(req,res)=>{
+User.findByIdAndUpdate(req.user._id,{$set:{pic:req.body.pic}}
+    ,{new:true},
+    (err,result)=>{
+        if(err)
+        {
+            return res.status(400).json({error:"pic cannot be posted"})
+        }
+        res.json(result)
+    })
+})
 
 module.exports =  router
